a182392a1a1a74248c55829568cf9aeca63bb2b5
kirankumar949489 <kirankumar949489@gmail.com>
2025-03-25 17:21:45 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
-import HomePage from "./home-page/HomePage";
-import Header from "./Shared/Header";
-import Footer from "./Shared/Footer";
+"use client";
 
-export default function Home() {
-  
+import React, { useState } from "react";
+import Header from "./Shared/Header";
+import HomePage from "./home-page/HomePage";
+
+export default function Page() {
+  const [headerSearchQuery, setHeaderSearchQuery] = useState("");
+  const [activeSearchQuery, setActiveSearchQuery] = useState("");
+  const [isHomeSearchVisible, setIsHomeSearchVisible] = useState(true);
+
   return (
-    <div className=" min-h-screen font-open">
-      <Header/>
-      <main className="flex flex-col   lg:px-[40px] pt-[60px] pb-[20px]">
-      <HomePage/>
+    <div className="min-h-screen font-open">
+      <Header
+        isHomeSearchVisible={isHomeSearchVisible}
+        headerSearchQuery={headerSearchQuery}
+        setHeaderSearchQuery={setHeaderSearchQuery}
+        setActiveSearchQuery={setActiveSearchQuery}
+      />
+      <main className="flex flex-col lg:px-[40px] pt-[60px] pb-[20px]">
+        <HomePage />
       </main>
-      <Footer/>
     </div>
   );
 }
